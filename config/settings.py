60b8c94feb9eb62ffe907f771a537a2c919ddc53
Eluid Maina <Eluid.Maina@womensbank.co.zw>
2020-03-20 12:38:24 +0200

    adding auth

--- config/settings.py
+++ config/settings.py
@@ -120,4 +120,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+# Redirect to home URL after login (Default redirects to /accounts/profile/)
+LOGIN_REDIRECT_URL = '/'
 
+
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
